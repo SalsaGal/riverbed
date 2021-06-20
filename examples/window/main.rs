4d3bd319cb9670782a267848a76625f6c01b9e6c
Salsa Gal <fielddashos@gmail.com>
2021-06-20 17:56:50 +1000

    Add basic window code

---
+++ examples/window/main.rs
@@ -0,0 +1,6 @@
+use riverbed::Game;
+
+fn main() {
+	let game = Game::new("Window");
+	game.run();
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,35 @@
+pub mod math;
+
+use winit::event::{Event, WindowEvent};
+use winit::event_loop::{ControlFlow, EventLoop};
+use winit::window::{Window, WindowBuilder};
+
 pub struct Game {
+	event_loop: EventLoop<()>,
+	window: Window,
+}
+
+impl Game {
+	pub fn new(title: &str) -> Self {
+		let event_loop = EventLoop::new();
+		let window = WindowBuilder::new().with_title(title);
+
+		let window = window.build(&event_loop).unwrap();
+
+		Self {
+			event_loop,
+			window,
+		}
+	}
+
+	pub fn run(self) {
+		self.event_loop.run(move |event, _, control_flow| {
+			match event {
+				Event::WindowEvent { event: WindowEvent::CloseRequested, .. } => {
+					*control_flow = ControlFlow::Exit;
+				},
+				_ => {},
+			}
+		});
+	}
 }
